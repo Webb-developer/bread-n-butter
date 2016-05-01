19904abffba405a2c475312f3f1b10b5d0d300a4
Andrew Puig <andrew.lpuig@gmail.com>
2016-05-01 11:02:24 -0400

    Misc Improvements
    Added documentation, added more SASS maps for bbui objects.
    Improvements to BBJS utilities

--- js/_bbjs-utilities.js
+++ js/_bbjs-utilities.js
@@ -9,7 +9,7 @@ Smooth anchor link scrolling.
 
 
     // a href contains #something not just #
-    $('a[href*="#"]:not([href="#"])').on("click", function(e) {
+    $('a[href*="#"]:not([href="#"])').on("click", function(e){
 
         e.preventDefault();
 
@@ -28,48 +28,87 @@ Smooth anchor link scrolling.
 /**
 @description
 JS version for CSS vh unit.
+This function will set an element's height or max height
+to x% of the window's height.
 
 @example
-<div class='js-set-vh' data-vh='70' set-max-height='true'><div>
+<div class='js-set-vh' data-vh='70'><div>
 This will set the element's height to 70% of
 the window height. If the data-vh attribute is missing
 or 0, the height of 100% of the window will be set.
+
+@example
+<div class='js-set-vh' data-vh='100' set-max-height><div>
+This will set the element's max height to 100% of
+the window height.
 */
 
-(function(){
+(function(window){
 
     'use strict';
 
 
-    var $item = $(".js-set-vh");
+    var settings = {
+
+        element: {
+            /**
+            @property {object} settings.element.$element
+            Selector for the element we are modifying.
+            */
+            $element: $(".js-set-vh"),
+
+            /**
+            @property {string} settings.element.dataVh
+            Data attribute whose value specifies the height percentage.
+            */
+            dataVh: "vh",
+
+            /**
+            @property {string} settings.element.setMaxHeightAttribute
+            Attribute, if present on settings.element.$element
+            will set the element's max height rather than height.
+            */
+            setMaxHeightAttribute: "set-max-height"
+        },
+
+        /**
+        @property {number} settings.debounceRate
+        The debounce rate for the window resize event.
+        */
+        debounceRate: 50
+
+    };
 
 
-    if($item.length){
+    if(settings.element.$element.length){
 
         var setVh = debounce(function(){
 
-            $item.each(function(index, el){
+            settings.element.$element.each(function(index, el){
 
-                var percent = $(el).data("vh") || 100,
+                var percent = $(el).data(settings.element.dataVh) || 100,
                     calc    = $(window).outerHeight() * (percent / 100);
 
 
-                // Double equals here for a less strict match.
-                if($el.attr("set-max-height")){
+                if($(el).attr(settings.element.setMaxHeightAttribute)){
+
                     $(el).css({
                         'max-height': calc,
                         'overflow-y': "hidden"
                     });
+
                 } else {
+
                     $(el).css({
                         'height': calc,
                         'overflow-y': "hidden"
                     });
+
                 }
 
             });
 
-        }, 50);
+        }, settings.debounceRate);
 
 
         setVh();
@@ -79,7 +118,7 @@ or 0, the height of 100% of the window will be set.
         });
     }
 
-}());
+}(window));
 
 
 
@@ -262,18 +301,26 @@ Clear input placeholders on focus.
 /**
 @description
 This function adds basic toggleClass() functionality
-on a click event to an element (settings.selector).
+on a click event to an element (settings.element.selector).
 
-The element (settings.selector) should have an attribute (settings.classes)
-which specifies which classes to add to the given element.
+The element should have an attribute (settings.element.dataToggle)
+which specifies the classes to add to either the root element
+or the clicked element's parent.
 
-By default this function will toggleClass() on the html element.
-If the element has the attribute toggle-parent, this function will apply
-the class to the parent of the clicked element.
+By default this function will toggle classes on the root element.
+If the element has the attribute settings.element.toggleParent, this function will apply
+the class to the parent of the clicked element. To help prevent page
+reflow, always try to apply the settings.element.toggleParent attribute.
 
 @example
 <div class='js-toggle' data-toggle='search'></div>
 Would output: "search--toggled" class on the html element.
+
+@example
+<div>
+    <div class='js-toggle' data-toggle='dropdown' toggle-parent></div>
+</div>
+Would output: "dropdown--toggled" class on the parent.
 */
 
 (function(){
@@ -284,13 +331,30 @@ Would output: "search--toggled" class on the html element.
     var settings = {
 
         element: {
+            /**
+            @property {string} settings.element.selector
+            The selector that will be bound to the click event.
+            */
             selector: ".js-toggle",
 
-            // element.selector attributes
-            classes: "data-toggle",
-            parentAttribute: "use-parent"
+            /**
+            @property {string} settings.element.dataToggle
+            The data attribute on the element.selector
+            that contains the classes we will toggle.
+            */
+            dataToggle: "toggle",
+
+            /**
+            @property {string} settings.element.toggleParent
+            */
+            toggleParent: "toggle-parent"
         },
 
+        /**
+        @property {string} settings.toggledClass
+        String that that will be appended to the
+        class. Consider this a BEM modifier.
+        */
         toggledClass: "--toggled"
 
     };
@@ -298,25 +362,36 @@ Would output: "search--toggled" class on the html element.
 
     $(document).on("click", settings.element.selector, function(){
 
+        // Save our this reference.
         var $self = $(this);
 
 
-        if($self.attr(settings.element.classes)){
+        // Check if the clicked element has the requried
+        // settings.element.dataToggle attribute.
+        if($self.data(settings.element.dataToggle)){
 
-            var classes = $self.attr(settings.element.classes).split(" ");
+            // Split the classes in the settings.element.dataToggle
+            // attribute by a single space. We do this because the user
+            // may want to toggle multiple classes.
+            var classes = $self.data(settings.element.dataToggle).split(" ");
 
-            $.each(classes, function(index, element){
 
-                if($self.attr(settings.element.parentAttribute) !== "true"){
-                    bbjs.cache.$html.toggleClass(element.concat(settings.toggledClass));
+            // Loop through each of the classes in the 
+            classes.forEach(function(attrClass){
+
+                // Toggle the immediate parent.
+                if($self.attr(settings.element.toggleParent)){
+                    $self.parent().toggleClass(attrClass.concat(settings.toggledClass));
                 } else {
-                    $self.parent().toggleClass(element.concat(settings.toggledClass));
+                    bbjs.cache.$html.toggleClass(attrClass.concat(settings.toggledClass));
                 }
 
             });
 
         } else {
-            throw new Error("Missing " + settings.element.classes + " attribute on " + settings.element.selector);
+            // Throw an error if the required settings.element.dataToggle
+            // attribute is missing.
+            throw new Error("Missing " + settings.element.dataToggle + " attribute on " + settings.element.selector);
         }
 
     });
